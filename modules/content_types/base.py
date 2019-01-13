adc48267174d43c476096ccbf5a9d2d90f57ff75
Thalida Noel <thalida.c.noel@gmail.com>
2019-01-13 16:54:01 -0500

    Removed stub content type classes

--- modules/content_types/base.py
+++
@@ -1,9 +0,0 @@
-from pprint import pprint
-from abc import ABC, abstractmethod
-
-class ContentTypes_Base(ABC):
-    
-    @abstractmethod
-    def do_something(self):
-        print("Some implementation!")
-
