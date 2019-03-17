8cdf8697d20044192fd26011e85fa6ab105bfff4
Thalida Noel <thalida.c.noel@gmail.com>
2019-03-17 19:27:23 -0400

    Check if job exits before creating one

--- app/unpack/__init__.py
+++ app/unpack/__init__.py
@@ -26,49 +26,56 @@ socketio = SocketIO(message_queue='redis://')
 # medium_weird_tree = 1049037454710394881
 # large_weird_tree = 946823401217380358
 # deleted_quoted_tweet = 946795191784132610
-# example_status_id = thread_example
-# example_status_id = quoted_example
-# example_status_id = multi_quote_example
-# example_status_id = simple_weird_tree
-# example_status_id = medium_weird_tree
-# example_status_id = large_weird_tree
-# example_status_id = deleted_quoted_tweet
-# example_url = f'https://twitter.com/i/web/status/{example_status_id}'
 
 # add reddit using their api
 # add insta (check if they have one?)
 # add any url (every single href on the page + the meta data)
 
 class Unpack:
+    URL_TYPES = [
+        TypeTwitter(),
+        TypeMedia(),
+        TypeBase(),
+    ]
+
+    tree = None
+    job = None
+
     def __init__(self, url):
-        self.db = {
+        self.url = url
+        self.url_hash = hash_url(self.url)
+        self.DB = {
             'host': os.getenv(ENV_VARS['DB']['HOST'], 'localhost'),
             'dbname': os.getenv(ENV_VARS['DB']['NAME']),
             'user': os.getenv(ENV_VARS['DB']['USER']),
             'password': os.getenv(ENV_VARS['DB']['PASSWORD'], None),
         }
-        self.url_types = [
-            TypeTwitter(),
-            TypeMedia(),
-            TypeBase(),
-        ]
-        self.tree = None
-        self.url = url
-        self.url_hash = hash_url(self.url)
         self.EVENT_KEYS = {
+            'TREE_INIT': f'tree_init:{self.url_hash}',
             'TREE_UPDATE': f'tree_update:{self.url_hash}',
         }
-        self.job = q.enqueue(self.run)
+        self.start_job()
+
+    def start_job(self):
+        found_job = self.__get_job(self.url_hash)
+        if found_job is not None:
+            self.job = found_job
+        else:
+            self.job = q.enqueue(self.run)
+            self.job.meta['url_hash'] = self.url_hash
+            self.job.save_meta()
+
+        return self.job
 
     def run(self):
-        self.tree = self.get_tree()
+        self.tree = self.__get_tree()
 
         if not self.tree[0]['is_from_db']:
             self.__store_tree(self.tree)
 
         self.__broadcast()
 
-    def get_tree(self):
+    def __get_tree(self):
         stored_parent_node = self.__load_node(self.url_hash)
         if stored_parent_node is not None:
             tree = self.__load_tree(self.url_hash)
@@ -80,11 +87,10 @@ class Unpack:
         return tree
 
     def __broadcast(self):
-        print(self.EVENT_KEYS['TREE_UPDATE'])
         socketio.emit(self.EVENT_KEYS['TREE_UPDATE'], self.tree)
 
-    def __execute(self, fetch_action, query, query_args):
-        with psycopg2.connect(**self.db) as conn:
+    def __execute_sql(self, fetch_action, query, query_args):
+        with psycopg2.connect(**self.DB) as conn:
             with conn.cursor(cursor_factory=RealDictCursor) as cur:
                 cur.execute(query, query_args)
                 fetch_fn = getattr(cur, fetch_action)
@@ -94,7 +100,7 @@ class Unpack:
 
     def __load_node(self, url_hash):
         try:
-            node = self.__execute(
+            node = self.__execute_sql(
                 'fetchone',
                 """
                 SELECT *
@@ -109,7 +115,7 @@ class Unpack:
 
     def __load_branches(self, parent_url_hash):
         try:
-            node = self.__execute(
+            node = self.__execute_sql(
                 'fetchall',
                 """
                 SELECT *
@@ -132,6 +138,7 @@ class Unpack:
             return tree
 
         raw_branches = self.__load_branches(url_hash)
+        # TODO: get node type to determine Type class used, see __fetch_tree
         node = TypeBase.setup_node(
             raw_node['url'],
             data=raw_node['data'],
@@ -159,7 +166,7 @@ class Unpack:
         node_type_cls = None
         node_id = None
 
-        for url_type in self.url_types:
+        for url_type in self.URL_TYPES:
             matches = url_type.URL_PATTERN.findall(branch['url'])
             if len(matches) > 0:
                 node_type_cls = url_type
@@ -191,7 +198,7 @@ class Unpack:
                 json.dumps(node_obj['data']),
                 node_obj['is_error'],
             )
-            node = self.__execute('fetchone', node_query, node_data)
+            node = self.__execute_sql('fetchone', node_query, node_data)
 
             if parent_node is not None:
                 branch_query = """
@@ -200,11 +207,23 @@ class Unpack:
                         RETURNING *
                         """
                 branch_data = (parent_node['url_hash'], node_obj['url_hash'], node_obj['relationship'])
-                branch = self.__execute('fetchone', branch_query, branch_data)
+                branch = self.__execute_sql('fetchone', branch_query, branch_data)
 
             return node
         except Exception:
             self.raise_error('Error inserting a new node: {node}', node=node_obj)
+
+    @staticmethod
+    def __get_job(url_hash):
+        found_job = None
+        queued_jobs = q.jobs
+
+        for job in queued_jobs:
+            if job.meta.get('url_hash') == url_hash:
+                found_job = job
+                break;
+
+        return found_job
 
     @staticmethod
     def raise_error(msg, **kwargs):
